ad65dbb158cd12c1e69741ac339998877e45fadb
TheCompSciNoob <kyroschow@gmail.com>
2019-08-08 09:55:59 -0700

    Changed code styles

---
+++ hw1/cmake-build-debug-cse-dev-c/CMakeFiles/hw1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw1.dir/hw1.c.obj"
+  "hw1.pdb"
+  "hw1.exe"
+  "hw1.exe.manifest"
+  "libhw1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hw1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
