0e8996f737f60c9e41cb2f2c0b99e69a9974cfe8
TheCompSciNoob <kyroschow@gmail.com>
2019-08-13 12:07:01 -0700

    Initial commit ec_hw2a

--- ec_hw2a/ec_hw2a.c
+++ ec_hw2a/ec_hw2a.c
@@ -7,155 +7,105 @@
  * Customize a pizza and calculate the cost of tye pizza based on the number of toppings selected.
  */
 
-#define _USE_MATH_DEFINES
 #include <stdio.h>
-#include <math.h>
 
-//Function prototypes
-void displayMenu();
+#define PIZZA_COST 10.0
+#define TOPPING_COST 1.25
 
-char getCharInput();
+/*
+ * Map option names to actions
+ * e.g. "add extra cheese" to "adding extra cheese"
+ */
+struct Option
+{
+    char name[25];
+    char action[25];
+};
 
-double getDoubleInput();
-
-double areaSquare(double width);
-
-double areaRectangle(double width, double height);
-
-double areaCircle(double radius);
-
-double surfaceAreaCube(double width);
-
-double surfaceAreaCylinder(double radius, double height);
-
-double volumeCube(double width);
-
+/*
+ * Customize a pizza and calculate the cost of tye pizza based on the number of toppings selected.
+ */
 int main()
 {
-    _Bool done = 0;
-    while (!done)
+    printf("Pizza! Pizza!\n");
+    printf("Cheese pizza costs $%.2f.", PIZZA_COST);
+    printf("Additional toppings cost $%.2f each.\n\n", TOPPING_COST);
+
+    //list of options in an array
+    //map name of option to action of option, e.g. "add extra cheese" to "adding extra cheese"
+    struct Option options[] = {
+            {.name = "Place Order", .action = "Placing Order"},
+            {.name = "Remove all toppings", .action = "All toppings removed"},
+            {.name = "Add extra cheese", .action = "Adding extra cheese"},
+            {.name = "Add pepperoni", .action = "Adding pepperoni"},
+            {.name = "Add sausage", .action = "Adding sausage"},
+            {.name = "Add bacon", .action = "Adding bacon"},
+            {.name = "Add mushrooms", .action = "Adding mushrooms"},
+            {.name = "Add pineapple", .action = "Add pineapple"},
+            {.name = "Add bell peppers", .action = "Add bell peppers"},
+            {.name = "Add olives", .action = "Adding olives"}
+    };
+    int NUM_OPTIONS = sizeof(options) / sizeof(options[0]); //size of array calculation
+
+    //pizza order cost calculation
+    int orderChoice = -1, numToppings = 0;
+    double totalCost = PIZZA_COST;
+    while (1)
     {
-        displayMenu();
-        char calculatorChoice = getCharInput();
-        double width = 0, height = 0, radius = 0;
-        switch (calculatorChoice)
+        //Print out possible options with for loop
+        int optionIndex;
+        for (optionIndex = 0; optionIndex < NUM_OPTIONS; optionIndex++)
         {
-            case 'q':
-            case 'Q':
-                done = 1;
-                break;
-            case '1':
-                printf("Enter width: ");
-                width = getDoubleInput();
-                printf("Result: %.2f meters\n\n", areaSquare(width));
-                break;
-            case '2':
-                printf("Enter width: ");
-                width = getDoubleInput();
-                printf("Enter height: ");
-                height = getDoubleInput();
-                printf("Result: %.2f meters\n\n", areaRectangle(width, height));
-                break;
-            case '3':
-                printf("Enter radius: ");
-                radius = getDoubleInput();
-                printf("Result: %.2f meters\n\n", areaCircle(radius));
-                break;
-            case '4':
-                printf("Enter width: ");
-                width = getDoubleInput();
-                printf("Result: %.2f meters\n\n", surfaceAreaCube(width));
-                break;
-            case '5':
-                printf("Enter radius: ");
-                radius = getDoubleInput();
-                printf("Enter height: ");
-                height = getDoubleInput();
-                printf("Result: %.2f meters\n\n", surfaceAreaCylinder(radius, height));
-                break;
-            case '6':
-                printf("Enter width: ");
-                width = getDoubleInput();
-                printf("Result: %.2f meters\n\n", volumeCube(width));
-                break;
-            default:
-                printf("Invalid choice\n\n");
-                break;
+            printf("%d) %s\n", optionIndex, options[optionIndex].name);
+        }
+
+        //Ask user for input and process order
+        printf("\nEnter choice: ");
+        scanf("%d", &orderChoice);
+        getchar();
+        if (orderChoice == 0)
+        {
+            //exit loop if user chooses place order
+            printf("Current price: %.2f\n\n", totalCost);
+            break;
+        } else if (orderChoice == 1)
+        {
+            //remove all toppings
+            numToppings = 0;
+            totalCost = PIZZA_COST;
+            printf("%s\n", options[orderChoice].action);
+            printf("Current price: %.2f\n\n", totalCost);
+        } else if (orderChoice >= 2 && orderChoice < NUM_OPTIONS)
+        {
+            //add topping
+            numToppings++;
+            totalCost += TOPPING_COST;
+            printf("%s\n", options[orderChoice].action);
+            printf("Current price: %.2f\n\n", totalCost);
+        } else
+        {
+            //catch invalid inputs
+            printf("Invalid choice\n\n");
         }
     }
 
-    //exit
-    printf("Goodbye!\n\n");
+    //Print receipt
+    printf("You are adding %d extra topping(s) for a total of %.2f\n", numToppings, totalCost);
+    printf("Do you wish to order this pizza (y/n)? ");
+    char shouldOrder;
+    scanf("%c", &shouldOrder);
+    getchar();
+    if (shouldOrder == 'y')
+    {
+        printf("\nThank you for your order, your pizza will be ready soon.\n\n");
+    } else
+    {
+        printf("\nYour order has bee canceled, please come again.\n\n");
+    }
+
+    //Keep the program alive to see the output
     printf("Press ENTER to exit!");
     getchar();
 
     return 0;
-}
-
-void displayMenu()
-{
-    //list of options
-    char options[][30] = { //20 = length of strings in array
-            "Area of a Square",
-            "Area of a Rectangle",
-            "Area of a Circle",
-            "Surface Area of a Cube",
-            "Surface Area of a Cylinder",
-            "Volume of a Cube"
-    };
-
-    //print options
-    printf("Geometry Calculator:\n");
-    unsigned int optionIndex;
-    for (optionIndex = 0; optionIndex < sizeof(options) / sizeof(options[0]); optionIndex++)
-    {
-        printf("%d) %s\n", optionIndex + 1, options[optionIndex]);
-    }
-    printf("\nEnter choice (Q/q) to quit: ");
-}
-
-char getCharInput()
-{
-    char input;
-    scanf("%c", &input);
-    getchar();
-    return input;
-}
-
-double getDoubleInput()
-{
-    double input;
-    scanf("%lf", &input);
-    getchar();
-    return input;
-}
-
-double areaSquare(double width)
-{
-    return pow(width, 2);
-}
-
-double areaRectangle(double width, double height)
-{
-    return width * height;
-}
-
-double areaCircle(double radius)
-{
-    return M_PI * pow(radius, 2);
-}
-
-double surfaceAreaCube(double width)
-{
-    return 6 * pow(width, 2);
-}
-
-double surfaceAreaCylinder(double radius, double height)
-{
-    return 2 * M_PI * pow(radius, 2) + 2 * M_PI * radius * height;
-}
-
-double volumeCube(double width)
-{
-    return pow(width, 3);
 }
