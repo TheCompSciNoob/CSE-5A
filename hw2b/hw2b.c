2344ba7888cb09d7f5659c084d15335c29c5a274
TheCompSciNoob <kyroschow@gmail.com>
2019-08-13 19:47:02 -0700

    Added comments

--- hw2b/hw2b.c
+++ hw2b/hw2b.c
@@ -1,13 +1,14 @@
 /*
- * File: hw2a.c
+ * File: hw2b.c
  * Name: Chi Chow
  * DateL August 13th, 2019
  * Sources of help: None
  *
- * Customize a pizza and calculate the cost of tye pizza based on the number of toppings selected.
+ * Calculate the area, surface area, and volume of different shapes.
  */
 
 #define _USE_MATH_DEFINES
+
 #include <stdio.h>
 #include <math.h>
 
@@ -30,6 +31,9 @@ double surfaceAreaCylinder(double radius, double height);
 
 double volumeCube(double width);
 
+/*
+ * Calculate the area, surface area, and volume of different shapes.
+ */
 int main()
 {
     _Bool done = 0;
@@ -42,49 +46,49 @@ int main()
         {
             case 'q':
             case 'Q':
-                done = 1;
+                done = 1; //end the loop if user enters Q/q
                 break;
-            case '1':
+            case '1': //area of square
                 printf("Enter width: ");
                 width = getDoubleInput();
                 printf("Result: %.2f meters\n\n", areaSquare(width));
                 break;
-            case '2':
+            case '2': //area of rectangle
                 printf("Enter width: ");
                 width = getDoubleInput();
                 printf("Enter height: ");
                 height = getDoubleInput();
                 printf("Result: %.2f meters\n\n", areaRectangle(width, height));
                 break;
-            case '3':
+            case '3': //area of circle
                 printf("Enter radius: ");
                 radius = getDoubleInput();
                 printf("Result: %.2f meters\n\n", areaCircle(radius));
                 break;
-            case '4':
+            case '4': //surface area of cube
                 printf("Enter width: ");
                 width = getDoubleInput();
                 printf("Result: %.2f meters\n\n", surfaceAreaCube(width));
                 break;
-            case '5':
+            case '5': //surface area of cylinder
                 printf("Enter radius: ");
                 radius = getDoubleInput();
                 printf("Enter height: ");
                 height = getDoubleInput();
                 printf("Result: %.2f meters\n\n", surfaceAreaCylinder(radius, height));
                 break;
-            case '6':
+            case '6': //volume of cube
                 printf("Enter width: ");
                 width = getDoubleInput();
                 printf("Result: %.2f meters\n\n", volumeCube(width));
                 break;
-            default:
+            default: //catch invalid inputs
                 printf("Invalid choice\n\n");
                 break;
         }
     }
 
-    //exit
+    //Keep the program alive to see the output
     printf("Goodbye!\n\n");
     printf("Press ENTER to exit!");
     getchar();
@@ -92,6 +96,9 @@ int main()
     return 0;
 }
 
+/*
+ * Display possible calculations.
+ */
 void displayMenu()
 {
     //list of options
@@ -114,6 +121,9 @@ void displayMenu()
     printf("\nEnter choice (Q/q) to quit: ");
 }
 
+/*
+ * Gets character input from user.
+ */
 char getCharInput()
 {
     char input;
@@ -122,6 +132,9 @@ char getCharInput()
     return input;
 }
 
+/*
+ * Gets double input from user.
+ */
 double getDoubleInput()
 {
     double input;
@@ -130,31 +143,49 @@ double getDoubleInput()
     return input;
 }
 
+/*
+ * Calculate area of square.
+ */
 double areaSquare(double width)
 {
     return pow(width, 2);
 }
 
+/*
+ * Calculate area of rectangle.
+ */
 double areaRectangle(double width, double height)
 {
     return width * height;
 }
 
+/*
+ * Calculate area of circle.
+ */
 double areaCircle(double radius)
 {
     return M_PI * pow(radius, 2);
 }
 
+/*
+ * Calculate surface area of cube.
+ */
 double surfaceAreaCube(double width)
 {
     return 6 * pow(width, 2);
 }
 
+/*
+ * Calculate surface area of cylinder.
+ */
 double surfaceAreaCylinder(double radius, double height)
 {
     return 2 * M_PI * pow(radius, 2) + 2 * M_PI * radius * height;
 }
 
+/*
+ * Calculate volume of cube.
+ */
 double volumeCube(double width)
 {
     return pow(width, 3);
