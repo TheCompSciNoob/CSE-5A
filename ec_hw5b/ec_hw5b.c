7cc6e973c05e2b1899fae6fea43ad253e6dc09ab
Chi Chow <kyroschow@gmail.com>
2019-09-06 15:16:25 -0700

    Added comments hw5b

--- ec_hw5b/ec_hw5b.c
+++ ec_hw5b/ec_hw5b.c
@@ -34,15 +34,16 @@ void encryptFile(char *fileName, int shift);
 int main(int argc, char *argv[])
 {
     int shift;
-    char fileSwitch[] = "-f:";
+    char fileSwitch[] = "-f:"; //detect a shift when the string is matched
     switch (argc)
     {
         case 2:
-            if (sscanf(argv[1], "%d", &shift) != 1)
+            if (sscanf(argv[1], "%d", &shift) != 1) //did not enter an int for shift
             {
                 printErrorMessages(ERROR_MSG_INV_INT);
                 exit(1);
             }
+            //print 4 lines of the cipher to demonstrate how it works
             printCipher(0, 1);
             printCipher(shift, 1);
             printf("\n");
@@ -50,19 +51,23 @@ int main(int argc, char *argv[])
             printCipher(shift, 0);
             break;
         case 3:
-            if (sscanf(argv[1], "%d", &shift) != 1)
+            if (sscanf(argv[1], "%d", &shift) != 1) //did not enter an int for shift
             {
                 printErrorMessages(ERROR_MSG_INV_INT);
                 exit(1);
             }
-            if (*argv[2] != '-')
+            if (*argv[2] != '-') //did not enter a switch
             {
                 char result[MAX_STR];
                 encrypt(shift, argv[2], result);
                 printf("%s\n", argv[2]);
                 printf("%s\n", result);
             }
-            else if (strlen(argv[2]) >= strlen(fileSwitch))
+            else if (strlen(argv[2]) < strlen(fileSwitch)) //entered an invalid switch
+            {
+                printErrorMessages(ERROR_MSG_INV_SWITCH);
+                exit(1);
+            } else //entered a potential switch
             {
                 char possibleSwitch[MAX_STR];
                 int fileSwitchLength = strlen(fileSwitch);
@@ -72,13 +77,9 @@ int main(int argc, char *argv[])
                 {
                     encryptFile(argv[2] + fileSwitchLength, shift);
                 }
-            } else
-            {
-                printErrorMessages(ERROR_MSG_INV_SWITCH);
-                exit(1);
             }
             break;
-        default:
+        default: //incorrect number of arguments
             printErrorMessages(ERROR_MSG_NUM_ARGS);
             exit(1);
     }
@@ -86,19 +87,25 @@ int main(int argc, char *argv[])
     return 0;
 }
 
+/*
+ * Shows error message and prints usage of the program.
+ */
 void printErrorMessages(char *message)
 {
     printf("%s\n", message);
     printf("Usage: hw5b <letter shift> [-f:filename] [string]\n");
 }
 
+/*
+ * Prints all letters of the alphabet after shift.
+ */
 void printCipher(int shift, int lowerCase)
 {
     int i, length = strlen(LETTERS);
     while (shift < 0) shift += length; //convert to positive shift (easier to handle)
     for (i = 0; i < length; i++)
     {
-        if (lowerCase)
+        if (lowerCase) //handling lower and upper case letters
         {
             printf("%c", tolower(LETTERS[(i + shift) % length]));
         } else
@@ -109,6 +116,9 @@ void printCipher(int shift, int lowerCase)
     printf("\n");
 }
 
+/*
+ * Encrypt an input string into result according to shift.
+ */
 void encrypt(int shift, char *input, char *result)
 {
     int i, numLetters = strlen(LETTERS), inputLength = strlen(input);
@@ -116,17 +126,17 @@ void encrypt(int shift, char *input, char *result)
     for (i = 0; i < inputLength; i++)
     {
         int letterIndex = tolower(input[i]) - 'a';
-        if (letterIndex >= 0 && letterIndex < numLetters)
+        if (letterIndex >= 0 && letterIndex < numLetters) //check if it is a letter
         {
             char letter = LETTERS[(letterIndex + shift) % numLetters];
-            if (isupper(input[i]))
+            if (isupper(input[i])) //handling upper lower and upper case letters.
             {
                 result[i] = toupper(letter);
             } else if (islower(input[i]))
             {
                 result[i] = tolower(letter);
             }
-        } else
+        } else //do not shift if it is not a letter
         {
             result[i] = input[i];
         }
@@ -134,9 +144,12 @@ void encrypt(int shift, char *input, char *result)
     result[inputLength] = '\0';
 }
 
+/*
+ * Takes a file name as input and encrypts the file according to shift.
+ */
 void encryptFile(char *fileName, int shift)
 {
-    if (strlen(fileName) == 0)
+    if (strlen(fileName) == 0) //special case when only the switch is entered but not a file name
     {
         printErrorMessages(ERROR_MSG_INV_SWITCH);
         exit(1);
@@ -148,7 +161,7 @@ void encryptFile(char *fileName, int shift)
         exit(1);
     }
     char line[MAX_STR], encrypted[MAX_STR];
-    while (fgets(line, MAX_STR, pFile))
+    while (fgets(line, MAX_STR, pFile)) //read file, encrypt, and print result
     {
         encrypt(shift, line, encrypted);
         printf("%s", encrypted);
--- hw5b/hw5b.c
+++ hw5b/hw5b.c
@@ -32,11 +32,12 @@ int main(int argc, char *argv[])
     switch (argc)
     {
         case 2:
-            if (sscanf(argv[1], "%d", &shift) != 1)
+            if (sscanf(argv[1], "%d", &shift) != 1) //did not enter an int for shift
             {
                 printErrorMessages(ERROR_MSG_INV_INT);
                 return 1;
             }
+            //print 4 lines of the cipher to demonstrate how it works
             printCipher(0, 1);
             printCipher(shift, 1);
             printf("\n");
@@ -44,7 +45,7 @@ int main(int argc, char *argv[])
             printCipher(shift, 0);
             break;
         case 3:
-            if (sscanf(argv[1], "%d", &shift) != 1)
+            if (sscanf(argv[1], "%d", &shift) != 1) //did not enter an int for shift
             {
                 printErrorMessages(ERROR_MSG_INV_INT);
                 return 1;
@@ -54,7 +55,7 @@ int main(int argc, char *argv[])
             printf("%s\n", argv[2]);
             printf("%s\n", result);
             break;
-        default:
+        default: //incorrect number of arguments
             printErrorMessages(ERROR_MSG_NUM_ARGS);
             return 1;
     }
@@ -62,19 +63,25 @@ int main(int argc, char *argv[])
     return 0;
 }
 
+/*
+ * Shows error message and prints usage of the program.
+ */
 void printErrorMessages(char *message)
 {
     printf("%s\n", message);
     printf("Usage: hw5b <letter shift> [string]\n");
 }
 
+/*
+ * Prints all letters of the alphabet after shift.
+ */
 void printCipher(int shift, int lowerCase)
 {
     int i, length = strlen(LETTERS);
     while (shift < 0) shift += length; //convert to positive shift (easier to handle)
     for (i = 0; i < length; i++)
     {
-        if (lowerCase)
+        if (lowerCase) //handling lower and upper case letters
         {
             printf("%c", tolower(LETTERS[(i + shift) % length]));
         } else
@@ -85,6 +92,9 @@ void printCipher(int shift, int lowerCase)
     printf("\n");
 }
 
+/*
+ * Encrypt an input string into result according to shift.
+ */
 void encrypt(int shift, char *input, char *result)
 {
     int i, numLetters = strlen(LETTERS), inputLength = strlen(input);
@@ -92,17 +102,17 @@ void encrypt(int shift, char *input, char *result)
     for (i = 0; i < inputLength; i++)
     {
         int letterIndex = tolower(input[i]) - 'a';
-        if (letterIndex >= 0 && letterIndex < numLetters)
+        if (letterIndex >= 0 && letterIndex < numLetters) //check if it is a letter
         {
             char letter = LETTERS[(letterIndex + shift) % numLetters];
-            if (isupper(input[i]))
+            if (isupper(input[i])) //handling upper lower and upper case letters.
             {
                 result[i] = toupper(letter);
             } else if (islower(input[i]))
             {
                 result[i] = tolower(letter);
             }
-        } else
+        } else //do not shift if it is not a letter
         {
             result[i] = input[i];
         }
